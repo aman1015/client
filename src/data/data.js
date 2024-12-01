19c474f2d5a445ab4c82c51678bb9aebce3d890a
aman1015 <superaman200@.com>
2024-12-01 19:57:04 +0530

    first-commit

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import WorkIm from "../assets/projects/workImg.jpeg";
+
+
+export const data = [
+  {
+    id: 1,
+    name: "The Blog Web App",
+    image: WorkIm,
+    github: "https://github.com/aman1015/BlogApp22",
+  },
+
+  {
+    id: 3,
+    name: "File Sharing Web App",
+    image: WorkIm,
+    github: "https://github.com/aman1015/fileSharing",
+    live: "",
+  },
+
+  {
+    id: 3,
+    name: "Task Manager Web App",
+    image: WorkIm,
+    github: "https://github.com/aman1015/task",
+    live: "",
+  },
+];
